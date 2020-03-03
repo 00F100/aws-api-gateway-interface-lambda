2bd5085acc5c64dde37061f1d00951bf12a73b0d
Joao Moraes <joao.moraes@era-br.com>
2020-03-03 07:36:34 -0300

    fix interface name and add response signature

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-export * from './IAWSApiGatewayLambda';
+export interface Request {
+  version: number;
+  resource: string;
+  path: string;
+  httpMethod: string;
+  headers?: Headers;
+  multiValueHeaders?: MultiHeaders;
+  queryStringParameters?: QueryString;
+  multiValueQueryStringParameters?: MultiQueryString;
+  requestContext?: RequestContext;
+  pathParameters?: PathParameters;
+  stageVariables?: any;
+  body?: string;
+  isBase64Encoded?: boolean;
+}
+
+export interface RequestContext {
+  accountId: string;
+  apiId: string;
+  authorizer: any;
+  domainName: string;
+  domainPrefix: string;
+  extendedRequestId: string;
+  httpMethod: string;
+  identity: RequestIdentity;
+  path: string;
+  protocol: string;
+  requestId: string;
+  requestTime: string;
+  requestTimeEpoch: string;
+  resourceId: any;
+  resourcePath: string;
+  stage: string;
+}
+
+export interface RequestIdentity {
+  accessKey: string;
+  accountId: string;
+  caller: any;
+  cognitoAuthenticationProvider: any;
+  cognitoAuthenticationType: string;
+  cognitoIdentityId: string;
+  cognitoIdentityPoolId: string;
+  principalOrgId: string;
+  sourceIp: string;
+  user: any;
+  userAgent: string;
+  userArn: string;
+}
+
+export interface Response {
+  statusCode: number;
+  body: string;
+}
+
+export interface DefaultObjectSingleValue {
+  [name: string]: string;
+}
+
+export interface DefaultObjectMultiValue {
+  [name: string]: string[];
+}
+
+export interface Headers extends DefaultObjectSingleValue {
+}
+
+export interface QueryString extends DefaultObjectSingleValue {
+}
+
+export interface MultiHeaders extends DefaultObjectMultiValue {
+}
+
+
+export interface MultiQueryString extends DefaultObjectMultiValue {
+}
+
+export interface PathParameters extends DefaultObjectSingleValue {
+}
